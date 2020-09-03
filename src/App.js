ba41deea6e82faac78db53a73d1d7ce4b4b339ac
Fahrur Rifai <mfahrurrifai@gmail.com>
2020-09-03 15:39:41 +0700

    first init

--- src/App.js
+++ src/App.js
@@ -1,26 +1,47 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ display: 'flex', justifyContent: 'center', }}>
+      <div style={{ width: '50%', }}>
+        <h2 style={{ textAlign: 'center', }}>Form Pembelian Buah</h2>
+        <table style={{ border: 0, width: '100%', }}>
+          <tbody>
+            <tr>
+              <td style={{ fontWeight: 'bold', }}>Nama Pelanggan</td>
+              <td><input type="text" /></td>
+            </tr>
+            <tr>
+              <td style={{ fontWeight: 'bold', }}>Daftar Items</td>
+              <td>
+                <input type="checkbox" />
+                Semangka 
+                <br />
+                <input type="checkbox" />
+                Jeruk 
+                <br />
+                <input type="checkbox" />
+                Nanas 
+                <br />
+                <input type="checkbox" />
+                Salak 
+                <br />
+                <input type="checkbox" />
+                Anggur 
+                <br />
+              </td>
+            </tr>
+            <tr>
+              <td colSpan="2">
+                <button type="button">Kirim</button>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
     </div>
-  );
-}
-
-export default App;
+    );
+  }
+  
+  export default App;
